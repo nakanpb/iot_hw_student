170567e5bede51f91a658f40f832dd7198c56e62
nakanpb <65070058@kmitl.ac.th>
2024-07-18 16:14:58 +0700

    Revert

--- app.py
+++ app.py
@@ -33,24 +33,6 @@ app.add_middleware(
 
 # https://fastapi.tiangolo.com/tutorial/sql-databases/#crud-utils
 
-@router_v1.get('/books')
-async def get_books(db: Session = Depends(get_db)):
-    return db.query(models.Book).all()
-
-@router_v1.get('/books/{book_id}')
-async def get_book(book_id: int, db: Session = Depends(get_db)):
-    return db.query(models.Book).filter(models.Book.id == book_id).first()
-
-@router_v1.post('/books')
-async def create_book(book: dict, response: Response, db: Session = Depends(get_db)):
-    # TODO: Add validation
-    newbook = models.Book(title=book['title'], author=book['author'], year=book['year'], is_published=book['is_published'])
-    db.add(newbook)
-    db.commit()
-    db.refresh(newbook)
-    response.status_code = 201
-    return newbook
-
 @router_v1.get('/students')
 async def get_students(db: Session = Depends(get_db)):
     return db.query(models.student).all()
@@ -104,8 +86,6 @@ async def delete_student(student_id: int, response: Response, db: Session = Depe
         response.status_code = 500
         return {'message': f'Internal Server Error: {str(e)}'}
     
-
-
 # @router_v1.patch('/students/{student_id}')
 # async def update_student(student_id: int, student: dict, db: Session = Depends(get_db)):
 #     pass
